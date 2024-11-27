9472c8c1782eb241a5f4d5f2f00811081e4b90d6
Kaan <kaganakkaya2002@gmail.com>
2024-11-27 03:54:33 +0300

    Jwt configuration extension has been added.

---
+++ CafeAPI/Extensions/JwtExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace CafeAPI.Extensions
+{
+    public static class JwtExtensions
+    {
+        public static void ConfigureJwt(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer("simple-cafe-app",options => {
+                    options.TokenValidationParameters = new()
+                    {
+                        ValidateAudience = true,
+                        ValidateIssuer = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+
+                        ValidAudience = builder.Configuration["TokenOptions:Audience"],
+                        ValidIssuer = builder.Configuration["TokenOptions:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenOptions:SecurityKey"])),
+                        LifetimeValidator = (notBefore, expires, securityToken, validationParameters)
+                   => expires != null ? expires > DateTime.UtcNow : false,
+                    };
+                });
+        }
+        }
+    }
+
