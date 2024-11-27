e568b79ff003a163d67b5e7432cf699d0e306b66
Kaan <kaganakkaya2002@gmail.com>
2024-11-27 03:54:10 +0300

    Global Exception Handler has been added.

---
+++ CafeAPI/Extensions/ConfigureExceptionHandlerException.cs
@@ -0,0 +1,47 @@
+﻿using Application.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Net.Mime;
+using System.Text.Json;
+
+namespace CafeAPI.Extensions
+{
+    public static class ConfigureExceptionHandlerException
+    {
+        public static void ConfigureExceptionHandler(this WebApplication builder)
+        {
+            builder.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    context.Response.ContentType = MediaTypeNames.Application.Json;
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    var statusCode = contextFeature.Error switch
+                    {
+                        ClientSideException => 400,
+                        BadRequestException => 502,
+                        UnAuthorizedException => 401,
+                        InternalServerException => 500,
+                        NotFoundException => 404
+                    };
+
+                    context.Response.StatusCode = statusCode;
+
+                    if(contextFeature!= null)
+                    {
+                        await context.Response.WriteAsync(JsonSerializer.Serialize(new
+                        {
+                            StatusCode = statusCode,
+                            Message = contextFeature.Error.Message,
+                            Title = "Hata alındı"
+                        }));
+                    }
+                    
+                });
+            }
+                );
+        }
+    }
+}
